602dd0aa1324e8ac9a8537665a3b62da6df284ed
Marco Berardini <mberardini@evolution-man.com>
2016-12-27 14:21:22 -0800

    [client] Built out search functionality for SearchComponent.

--- src/app/components/search.component.ts
+++ src/app/components/search.component.ts
@@ -24,7 +24,45 @@ import {SpotifyService} from "../services/spotify.service";
       </span>
      </div><!-- input-group -->
   
+    <div *ngIf="results">
+      <div *ngIf="!results.length">
+        No tracks were found with the term '{{ query }}'
+      </div><!-- ngIf message NO TRACKS --> 
+      
+      <div *ngIf="results.length">
+        <h1>Results</h1>
   
+        <div class="row">
+          <div class="col-sm-6 col-md-4" *ngFor="let t of results"><!-- iterate over results-->
+            <div class="thumbnail">
+              <div class="content">
+                <img src="{{ t.album.images[0].url }}" class="img-responsive">
+                <div class="caption">
+                  <h3>
+                    <a [routerLink]="['/artists', t.artists[0].id]">
+                      {{ t.artists[0].name }}
+                    </a>
+                  </h3>
+                  <br>
+                  <p>
+                    <a [routerLink]="['/tracks', t.id]">
+                      {{ t.name }}
+                    </a>
+                  </p>
+                </div><!-- caption -->
+                <div class="attribution">
+                  <h4>
+                    <a [routerLink]="['/albums', t.album.id]">
+                      {{ t.album.name }}
+                    </a>
+                  </h4>
+                </div><!--attribution -->
+              </div><!-- content-->
+            </div><!-- thumbnail -->
+          </div><!-- ngFor col-6-sm and 4-md -->
+        </div><!-- row -->
+      </div><!-- result length-->
+    </div> <!-- results ngIF -->
   
   `
   ,
@@ -32,7 +70,34 @@ import {SpotifyService} from "../services/spotify.service";
 export class SearchComponent {
   query: string;
   results: Object;
-  constructor(private spotifyService: SpotifyService){
 
-  }
+  constructor(private spotifyService: SpotifyService,
+              private router: Router,
+              private route: ActivatedRoute
+            ){
+    this.route
+        .queryParams
+        .subscribe(params => { this.query = params['query'] || ''; });
+    }
+
+    ngOnInit(){
+      this.search();
+    }
+
+    submit(query: string): void {
+      this.router.navigate(['search'], { queryParams: { query: query } })
+          .then(_ => this.search() );
+    }
+
+    search(): void {
+      console.log('this.query', this.query);
+      if (!this.query) {
+        return;
+      }
+
+      this.spotifyService
+          .searchTrack(this.query)
+          .subscribe((res: any) => this.renderResults(res));
+    }
+
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -8,7 +8,7 @@ export class SpotifyService {
   constructor( private http: Http){
 
   }
-  searchByTrack(query: string){
+  searchTrack(query: string){
     let params: string = [`q=${query}`, `type=track` ].join('&');
     let queryURL: string = `https://api.spotify.com/v1/search?${params}`;
     return this.http.request(queryURL)
