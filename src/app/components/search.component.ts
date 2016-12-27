2a0b4b2c86d04c67fb2d655f4cd7a0c201747910
Marco Berardini <mberardini@evolution-man.com>
2016-12-27 13:55:34 -0800

    [client] Added Searchbox to SearchComponent.

--- src/app/components/search.component.ts
+++ src/app/components/search.component.ts
@@ -7,10 +7,31 @@ import {SpotifyService} from "../services/spotify.service";
 
 @Component({
   selector: 'app-search',
-  template: `<h1>search</h1>`,
+  template: `
+    <h1>Search</h1>
+
+ 
+    <div class="input-group">
+      <input 
+        #newquery
+        [value]="query"
+        (keydown.enter)="submit(newquery.value)"
+        type="text"
+        class="form-control" 
+        placeholder="Search for..."/>
+      <span class="input-group-btn">
+        <button (click)="submit(newquery.value)" class="btn btn-default" type="button">Search</button>
+      </span>
+     </div><!-- input-group -->
+  
+  
+  
+  `
+  ,
 })
 export class SearchComponent {
-
+  query: string;
+  results: Object;
   constructor(private spotifyService: SpotifyService){
 
   }
